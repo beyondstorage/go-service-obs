84387bf78368ebe02f718e2ffcf332714a0fbe65
abyss-w <75366604+abyss-w@users.noreply.github.com>
2021-08-27 16:30:48 +0800

    feat: Initial Implementation of Storager (#3)

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/beyondstorage/go-service-example
+module github.com/beyondstorage/go-service-obs
 
 go 1.15
 
-require github.com/beyondstorage/go-storage/v4 v4.6.0
+require (
+	github.com/beyondstorage/go-endpoint v1.1.0
+	github.com/beyondstorage/go-storage/v4 v4.6.0
+	github.com/huaweicloud/huaweicloud-sdk-go-obs v3.21.1+incompatible
+)
--- utils.go
+++ utils.go
@@ -1,12 +1,38 @@
 package obs
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-obs/obs"
+
+	"github.com/beyondstorage/go-endpoint"
+	ps "github.com/beyondstorage/go-storage/v4/pairs"
+	"github.com/beyondstorage/go-storage/v4/pkg/credential"
 	"github.com/beyondstorage/go-storage/v4/services"
 	"github.com/beyondstorage/go-storage/v4/types"
 )
 
-// Storage is the example client.
+type Service struct {
+	service *obs.ObsClient
+
+	defaultPairs DefaultServicePairs
+	features     ServiceFeatures
+
+	types.UnimplementedServicer
+}
+
+func (s *Service) String() string {
+	return fmt.Sprintf("Servicer obs")
+}
+
+// Storage is the obs client.
 type Storage struct {
+	client *obs.ObsClient
+
+	bucket  string
+	workDir string
+
 	defaultPairs DefaultStoragePairs
 	features     StorageFeatures
 
@@ -15,23 +41,138 @@ type Storage struct {
 
 // String implements Storager.String
 func (s *Storage) String() string {
-	panic("implement me")
+	return fmt.Sprintf("Storager obs {Name: %s, WorkDir: %s", s.bucket, s.workDir)
+}
+
+func New(pairs ...types.Pair) (types.Servicer, types.Storager, error) {
+	return newServicerAndStorager(pairs...)
+}
+
+func NewServicer(pairs ...types.Pair) (types.Servicer, error) {
+	return newServicer(pairs...)
 }
 
 // NewStorager will create Storager only.
 func NewStorager(pairs ...types.Pair) (types.Storager, error) {
-	return newStorager(pairs...)
+	_, store, err := newServicerAndStorager(pairs...)
+	return store, err
 }
 
-// newStorager will create a storage client.
-func newStorager(pairs ...types.Pair) (store *Storage, err error) {
+func newServicer(pairs ...types.Pair) (srv *Service, err error) {
 	defer func() {
 		if err != nil {
-			err = services.InitError{Op: "new_storager", Type: Type, Err: formatError(err), Pairs: pairs}
+			err = services.InitError{
+				Op:    "new_servicer",
+				Type:  Type,
+				Err:   formatError(err),
+				Pairs: pairs,
+			}
 		}
 	}()
 
-	panic("implement me")
+	srv = &Service{}
+
+	opt, err := parsePairServiceNew(pairs)
+	if err != nil {
+		return nil, err
+	}
+
+	cp, err := credential.Parse(opt.Credential)
+	if err != nil {
+		return nil, err
+	}
+	if cp.Protocol() != credential.ProtocolHmac {
+		return nil, services.PairUnsupportedError{Pair: ps.WithCredential(opt.Credential)}
+	}
+	ak, sk := cp.Hmac()
+
+	ep, err := endpoint.Parse(opt.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	var url string
+	switch ep.Protocol() {
+	case endpoint.ProtocolHTTP:
+		url, _, _ = ep.HTTP()
+	case endpoint.ProtocolHTTPS:
+		url, _, _ = ep.HTTPS()
+	default:
+		return nil, services.PairUnsupportedError{Pair: ps.WithEndpoint(opt.Endpoint)}
+	}
+
+	srv.service, err = obs.New(ak, sk, url)
+	if err != nil {
+		return nil, err
+	}
+
+	if opt.HasDefaultServicePairs {
+		srv.defaultPairs = opt.DefaultServicePairs
+	}
+	if opt.HasServiceFeatures {
+		srv.features = opt.ServiceFeatures
+	}
+
+	return
+}
+
+func newServicerAndStorager(pairs ...types.Pair) (srv *Service, store *Storage, err error) {
+	srv, err = newServicer(pairs...)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	store, err = srv.newStorage(pairs...)
+	if err != nil {
+		err = services.InitError{Op: "new_storager", Type: Type, Err: formatError(err), Pairs: pairs}
+		return nil, nil, err
+	}
+
+	return
+}
+
+func (s *Service) newStorage(pairs ...types.Pair) (store *Storage, err error) {
+	opt, err := parsePairStorageNew(pairs)
+	if err != nil {
+		return nil, err
+	}
+
+	store = &Storage{
+		client:  s.service,
+		bucket:  opt.Name,
+		workDir: "/",
+	}
+
+	if opt.HasWorkDir {
+		store.workDir = opt.WorkDir
+	}
+	if opt.HasDefaultStoragePairs {
+		store.defaultPairs = opt.DefaultStoragePairs
+	}
+	if opt.HasStorageFeatures {
+		store.features = opt.StorageFeatures
+	}
+
+	return
+}
+
+const (
+	// writeSizeMaximum is the maximum size for write operation, 5GB.
+	// ref: https://support.huaweicloud.com/sdk-go-devg-obs/obs_23_0402.html
+	writeSizeMaximum = 5 * 1024 * 1024 * 1024
+)
+
+func (s *Service) formatError(op string, err error, name string) error {
+	if err == nil {
+		return nil
+	}
+
+	return services.ServiceError{
+		Op:       op,
+		Err:      formatError(err),
+		Servicer: s,
+		Name:     name,
+	}
 }
 
 func (s *Storage) formatError(op string, err error, path ...string) error {
@@ -54,5 +195,55 @@ func formatError(err error) error {
 		return err
 	}
 
-	panic("implement me")
+	e, ok := err.(obs.ObsError)
+	if ok {
+		switch e.Code {
+		case "AccessDenied":
+			return fmt.Errorf("%w, %v", services.ErrPermissionDenied, err)
+		case "NoSuchKey":
+			return fmt.Errorf("%w, %v", services.ErrObjectNotExist, err)
+		default:
+			return fmt.Errorf("%w, %v", services.ErrUnexpected, err)
+		}
+	}
+
+	return fmt.Errorf("%w, %v", services.ErrUnexpected, err)
+}
+
+// getAbsPath will calculate object storage's abs path
+func (s *Storage) getAbsPath(path string) string {
+	prefix := strings.TrimPrefix(s.workDir, "/")
+	return prefix + path
+}
+
+// getRelPath will get object storage's rel path.
+func (s *Storage) getRelPath(path string) string {
+	prefix := strings.TrimPrefix(s.workDir, "/")
+	return strings.TrimPrefix(path, prefix)
+}
+
+func (s *Storage) formatFileObject(v obs.Content) (o *types.Object, err error) {
+	o = s.newObject(false)
+	o.ID = v.Key
+	o.Path = s.getRelPath(v.Key)
+	o.Mode |= types.ModeRead
+
+	o.SetContentLength(v.Size)
+	o.SetLastModified(v.LastModified)
+
+	if v.ETag != "" {
+		o.SetEtag(v.ETag)
+	}
+
+	var sm ObjectSystemMetadata
+	if value := v.StorageClass; value != "" {
+		sm.StorageClass = string(value)
+	}
+	o.SetSystemMetadata(sm)
+
+	return
+}
+
+func (s *Storage) newObject(done bool) *types.Object {
+	return types.NewObject(s, done)
 }
