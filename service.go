35ffbc6e6c35d00e1196587a6e22cc26abf285b8
abyss-w <75366604+abyss-w@users.noreply.github.com>
2021-08-30 19:12:02 +0800

    feat: Initial Implementation of Servicer (#5)

--- service.go
+++ service.go
@@ -3,21 +3,76 @@ package obs
 import (
 	"context"
 
+	"github.com/huaweicloud/huaweicloud-sdk-go-obs/obs"
+
+	ps "github.com/beyondstorage/go-storage/v4/pairs"
 	. "github.com/beyondstorage/go-storage/v4/types"
 )
 
 func (s *Service) create(ctx context.Context, name string, opt pairServiceCreate) (store Storager, err error) {
-	panic("not implemented")
+	pairs := append(opt.pairs, ps.WithName(name))
+
+	st, err := s.newStorage(pairs...)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &obs.CreateBucketInput{
+		Bucket: name,
+	}
+
+	_, err = s.service.CreateBucket(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return st, nil
 }
 
 func (s *Service) delete(ctx context.Context, name string, opt pairServiceDelete) (err error) {
-	panic("not implemented")
+	_, err = s.service.DeleteBucket(name)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *Service) get(ctx context.Context, name string, opt pairServiceGet) (store Storager, err error) {
-	panic("not implemented")
+	pairs := append(opt.pairs, ps.WithName(name))
+
+	st, err := s.newStorage(pairs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return st, nil
 }
 
 func (s *Service) list(ctx context.Context, opt pairServiceList) (sti *StoragerIterator, err error) {
-	panic("not implemented")
+	input := &storagePageStatus{}
+
+	return NewStoragerIterator(ctx, s.nexStoragePage, input), nil
+}
+
+func (s *Service) nexStoragePage(ctx context.Context, page *StoragerPage) error {
+	input := &obs.ListBucketsInput{
+		QueryLocation: true,
+	}
+
+	output, err := s.service.ListBuckets(input)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range output.Buckets {
+		store, err := s.newStorage(ps.WithName(v.Name))
+		if err != nil {
+			return err
+		}
+
+		page.Data = append(page.Data, store)
+	}
+
+	return IterateDone
 }
